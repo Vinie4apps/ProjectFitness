c027c19d1226be3e2ab9e4089dfc6580de2c84f0
Vinícius <vinie4apps@gmail.com>
2021-03-27 10:23:10 -0300

    Project Fitness

---
+++ app/src/main/java/com/vinie4apps/projectfitness/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.vinie4apps.projectfitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(com.vinie4apps.projectfitness.SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },2000);
+
+
+    }
+}
