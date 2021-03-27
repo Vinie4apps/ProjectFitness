0b1848b039ed8e09f488b8800ebf94b1c5fd2525
Vinícius <vinie4apps@gmail.com>
2021-03-27 12:28:08 -0300

    Project Fitness

--- app/src/main/java/com/vinie4apps/projectfitness/MainActivity.java
+++ app/src/main/java/com/vinie4apps/projectfitness/MainActivity.java
@@ -2,40 +2,79 @@ package com.vinie4apps.projectfitness;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-
-                case R.id.dieta:
-                    startActivity(new Intent(MainActivity.this, dieta.class));
-                    return true;
-                case R.id.treino:
-                    startActivity(new Intent(MainActivity.this, treino.class));
-                    return true;
-            }
-            return false;
-        }
-    };
+    private PageAdapter pageAdapter;
+    private ViewPager viewPager;
+    private LinearLayout linearDot;
+    private TextView[] textView;
+    private int[] layouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        viewPager = findViewById(R.id.view_pager);
+
+
+        layouts = new int[]{R.layout.home_view,R.layout.activity_dieta,R.layout.activity_treino};
+        pageAdapter = new PageAdapter(layouts, getApplicationContext());
+        viewPager.setAdapter(pageAdapter);
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        tabs.setBackgroundColor(ContextCompat.getColor(this, R.color.azul));
+        tabs.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.white));
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.azul));
+        }
+
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+
+
     }
+
+
 }
